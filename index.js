064107b5aa349a8858d85ea590c0eb8ae946ddc3
PedroLucasLuiz1 <98857645+PedroLucasLuiz1@users.noreply.github.com>
2026-05-21 08:34:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ client.once("ready", () => {
     sendMessage(BOSS_NATUREZA_CHANNEL, "🌿 **BOSS NATUREZA EM 15 MINUTOS!**\n@everyone\nBoss Natureza começa às **19:00**! 🌿");
   }, { timezone: "America/Sao_Paulo" });
 
-  // Doações — todo dia às 12:00
-cron.schedule("0 12 * * *", () => {
+  // Doações — todo dia às 09:00
+cron.schedule("0 09 * * *", () => {
   sendMessage(DOACOES_CHANNEL, "💰 **LEMBRETE DE DOAÇÕES**\n@everyone\nContribuam com a guilda:\n• Posto avançado\n• Tecnologia");
 }, { timezone: "America/Sao_Paulo" });
 
